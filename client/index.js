269817dec05bfd493aff71ff8165774cbbc508f8
Thibaut Fatus <tfatus@gmail.com>
2016-12-29 18:51:36 +0100

    hmr ok

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+if (module.hot) {
+    module.hot.accept(); // eslint-disable-line global-require
+}
+
 ReactDOM.render(
-  <h1>Hello, world of React 2!</h1>,
+  <h1>Hello, world of React 3!</h1>,
   document.getElementById('root')
 );
