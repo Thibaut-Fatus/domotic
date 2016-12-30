4fc5ef6e618b8982bece3b9fd0dbf1b3556fedc4
Thibaut Fatus <tfatus@gmail.com>
2016-12-30 13:41:32 +0100

    eslint ok

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
 
 if (module.hot) {
     module.hot.accept(); // eslint-disable-line global-require
 }
 
 ReactDOM.render(
-  <h1>Hello, world of React 3!</h1>,
-  document.getElementById('root')
+    <Router history={browserHistory}>
+        {routes}
+    </Router>,
+    document.getElementById('root'),
 );
