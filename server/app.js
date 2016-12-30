a6088aef3e520545bdc4706cc720bcd9e6227122
Thibaut Fatus <tfatus@gmail.com>
2016-12-30 18:22:50 +0100

    open

--- server/app.js
+++ server/app.js
@@ -69,6 +69,6 @@ app.get('*', function (req, res) {
     res.redirect('/');
 });
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0', function () {
     console.log('Example app listening on port 3000!');
 });
