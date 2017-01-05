fd9b95e3a6c52fac6a6bc365cf0915bcb729f375
Thibaut <tfatus@gmail.com>
2017-01-05 22:25:46 +0000

    wip

--- server/app.js
+++ server/app.js
@@ -9,40 +9,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const redoid = Redoid({
-    color: '#300000',
-    loopTransition: true,
+    color: [0, 0, 0],
     colorComponentPins: [17, 18, 4],
 });
 
 let colors = {
-    r: 1,
-    g: 1,
-    b: 1,
+    r: 150,
+    g: 55,
+    b: 255,
 };
 
-redoid.transition([255 * colors.r, 255 * colors.g, 255 * colors.b], 5000);
+redoid.transition([colors.r, colors.g, colors.b], 500);
 
-if (module.hot) {
-    module.hot.accept();
+console.log(process.env);
+
+if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+        module.hot.accept();
+    }
+    (function() {
+    
+        // Step 1: Create & configure a webpack compiler
+        const webpack = require('webpack');
+        const webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../webpack.config');
+        const compiler = webpack(webpackConfig);
+    
+        // Step 2: Attach the dev middleware to the compiler & the server
+        app.use(webpackDevMiddleware(compiler, {
+            noInfo: true, publicPath: webpackConfig.output.publicPath
+        }));
+    
+        // Step 3: Attach the hot middleware to the compiler & the server
+        app.use(webpackHotMiddleware(compiler, {
+            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+        }));
+    })();
 }
-
-(function() {
-
-    // Step 1: Create & configure a webpack compiler
-    const webpack = require('webpack');
-    const webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../webpack.config');
-    const compiler = webpack(webpackConfig);
-
-    // Step 2: Attach the dev middleware to the compiler & the server
-    app.use(webpackDevMiddleware(compiler, {
-        noInfo: true, publicPath: webpackConfig.output.publicPath
-    }));
-
-    // Step 3: Attach the hot middleware to the compiler & the server
-    app.use(webpackHotMiddleware(compiler, {
-        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-    }));
-})();
 
 app.get('/', function (req, res) {
     res.sendFile('client/index.html', { root: '.' });
@@ -59,8 +61,11 @@ app.get('/api/colors', function (req, res) {
 
 app.post('/api/colors', function (req, res) {
     if (req.body.colors !== undefined) {
-        colors = req.body.colors;
-        redoid.transition([255 * colors.r, 255 * colors.g, 255 * colors.b], 3000);
+        colors = JSON.parse(req.body.colors);
+        console.log(colors);
+        console.log(redoid.isColorValid([colors.r, colors.g, colors.b]));
+        // redoid.transition([255 * colors.r, 255 * colors.g, 255 * colors.b], 500, 'easeInOutCubic');
+        redoid.change([colors.r, colors.g, colors.b]);
     }
     res.send(colors);
 });
